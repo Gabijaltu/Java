1854c2999fea78cd819235d68863b3435ea73e3f
Gabija <gabija.simu@gmail.com>
2025-02-24 11:46:39 +0200

    5.12+

---
+++ 4.3/Multiplication_Table/src/MultiplicationTable.java
@@ -0,0 +1,11 @@
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+    for (int firstNumber = 1; firstNumber < 10; firstNumber++) {
+       System.out.println();
+        for (int secondNumber = 1; secondNumber <10; secondNumber++) {
+            System.out.print(firstNumber*secondNumber + " ");
+        }
+    }
+    }
+}
---
+++ 4.3/Square/src/Square.java
@@ -0,0 +1,10 @@
+
+public class Square {
+    public static void main(String[] args) {
+        for (int size = 0; size < 8; size++) {
+            for (int minute = 0; minute < 60; minute++) {
+                System.out.println(hour + ":" + minute);
+            }
+        }
+    }
+}
