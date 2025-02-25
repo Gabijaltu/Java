9fa2b0972c35f80f846fcb3deb8fb01297e0a184
Gabija <gabija.simu@gmail.com>
2025-02-25 20:37:07 +0200

    9+

---
+++ ArrayList/AL_10.IndexOf/src/main/java/IndexOf.java
@@ -0,0 +1,37 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IndexOf {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+        System.out.println("");
+
+        // implement here finding the indices of a number
+        System.out.println("Search for?");
+
+        int searchingFor = Integer.valueOf(scanner.nextLine());
+
+
+        for (int i = 0; i < list.size(); i++) {
+           if (searchingFor == list.get(i)) {
+               int wantedIndex = list.indexOf(searchingFor);
+               System.out.println(searchingFor + " is at index " + wantedIndex);
+           }
+
+        }
+
+    }
+}
