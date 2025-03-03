192450eb1f4625adbe3bd7520f10881681d42355
Gabija <gabija.simu@gmail.com>
2025-03-03 11:58:00 +0200

    8.10+

--- 8.2/08_2_10.ComparingApartments/src/main/java/Apartment.java
+++ 8.2/08_2_10.ComparingApartments/src/main/java/Apartment.java
@@ -3,12 +3,35 @@ public class Apartment {
 
     private int rooms;
     private int squares;
-    private int princePerSquare;
+    private int pricePerSquare;
 
     public Apartment(int rooms, int squares, int pricePerSquare) {
         this.rooms = rooms;
         this.squares = squares;
-        this.princePerSquare = pricePerSquare;
+        this.pricePerSquare = pricePerSquare;
     }
+
+    public int getSquares() {
+        return squares;
+    }
+
+    public int getPricePerSquare() {
+        return pricePerSquare;
+    }
+
+    public boolean largerThan(Apartment compared) {
+        return getSquares() > compared.getSquares();
+    }
+
+    public int priceDifference(Apartment compared) {
+        if (getPricePerSquare() < compared.getPricePerSquare()) {
+            return compared.getPricePerSquare() * compared.getSquares() - getPricePerSquare() * getSquares();
+        }
+        return getPricePerSquare() * getSquares() - compared.getPricePerSquare() * compared.getSquares();
+    }
+    public boolean moreExpensiveThan(Apartment compared) {
+        return getPricePerSquare() > compared.getPricePerSquare();
+    }
+
 
 }
